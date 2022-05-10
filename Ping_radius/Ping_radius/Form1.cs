fee02b6101a826c57856030c7866b19faa9a74ea
Pavel <86227916+Pajiiik@users.noreply.github.com>
2022-05-10 14:11:18 +0200

    Add files via upload

---
+++ Ping_radius/Ping_radius/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace Ping_radius
+{
+    public partial class Form1 : Form
+    {
+        List<IPadresa> lIP = new List<IPadresa>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static void PingHost(IPadresa ipadresa)
+        {
+            bool pingnuto = false;
+            Ping pinger = null;
+
+            try
+            {
+                pinger = new Ping();
+                PingReply odpoved = pinger.Send(ipadresa.IP);
+                ipadresa.Alive = odpoved.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+            
+            }
+        }
+
+        int pocPrvni, pocDruha, pocTreti, pocCtvrta, prvni;
+        int endPrvni, endDruha, endTreti, endCtvrta, posledni;
+
+
+
+
+
+        //192.168.2.1 - 192.168.2.10
+        //inicializae listu - 
+        // listIP.Add( new IpAdresa{IP = 'kombinace', '', false});
+
+
+        //naplneni bude 
+        // foreach (var item in listIP)
+        // Pingni(item.Ip) // funkce která nastaví Alive
+        // item.Alive = true;
+
+        bool done  = true;
+
+    
+        private void Test()
+        {
+
+
+
+            string pocatekIP = string.Format("{0}.{1}.{2}.", pocPrvni,pocDruha,pocTreti);
+            string konecIP = string.Format("{0}.{1}.{2}.", pocPrvni, pocDruha, pocTreti);
+            int pocet; // rozdil mezi pocatek konec
+            pocet = endCtvrta - pocCtvrta;
+      
+                for (int i = 0; i <= pocet; i++)
+                {
+                lIP.Add(new IPadresa { IP = pocatekIP + pocCtvrta.ToString() });
+                // listBox1.Items.Add(PingHost(pocatekIP + pocCtvrta.ToString()));
+                    pocCtvrta++;
+                };
+
+            foreach (var item in lIP)
+            {
+                PingHost(item);
+                listBox1.Items.Add(item.IP + " - " + item.Alive.ToString());
+            }
+            int counter = 0;
+            //while (done)
+            //{
+            //   // Thread vlakno0 = new Thread(new ThreadStart(pocitadlo()));
+            //   // Thread vlakno1 = new Thread(new ThreadStart(pocitadlo()));
+            //   // Thread vlakno2 = new Thread(new ThreadStart(pocitadlo()));
+            //  //  Thread vlakno3 = new Thread(new ThreadStart(pocitadlo()));
+            //  //  Thread vlakno4 = new Thread(new ThreadStart(pocitadlo()));
+
+
+            //    counter++;
+            //    if (counter == 10)    counter = 0;
+            //}
+
+        }
+        private void filler()
+        {
+            pocPrvni = int.Parse(textBox1.Text);
+            pocDruha = int.Parse(textBox3.Text);
+            pocTreti = int.Parse(textBox5.Text);
+            pocCtvrta = int.Parse(textBox4.Text);
+
+            endPrvni = int.Parse(textBox8.Text);
+            endDruha = int.Parse(textBox7.Text);
+            endTreti = int.Parse(textBox6.Text);
+            endCtvrta = int.Parse(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            filler();
+            Test();
+        }
+    }
+}
---
+++ Ping_radius/Ping_radius/IPadresa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ping_radius
+{
+    public class IPadresa
+    {
+
+        public string IP { get; set; }
+
+        public string NameDevice { get; set; }
+
+        public bool Alive { get; set; }
+
+        // příklad výpočtu "proměné"
+       // public string CeleJmenno { get { return NameDevice + Alive.ToString(); } }
+
+
+
+    }
+}
